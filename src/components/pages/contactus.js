c72aa90291754d9521ff9900ca0d602777c4a763
Alli <eielelai.eielelai@gmail.com>
2021-05-20 23:25:55 +0800

    initial divs for Contact Us

--- src/components/pages/contactus.js
+++ src/components/pages/contactus.js
@@ -4,7 +4,12 @@ import "../../App.css";
 function ContactUs() {
   return (
     <>
-      <h1 className="contactus">Contact Us</h1>
+      <div>
+        <h1 className="contactus">Contact Us</h1>
+        <div>
+          <p>Want to have a weekend getaway? Book us now!</p>
+        </div>
+      </div>
     </>
   );
 }
